9cd33210dd9fdb97a3c4c56489cc8a648f486a68
AmitRanjanSahoo <87256782+AmitRanjanSahoo@users.noreply.github.com>
2025-04-10 22:16:53 +0530

    Modification
    Some elements are added and some are modified, the functionality has been upgraded.

--- 8532strom-gt9/main.js
+++ 8532strom-gt9/main.js
@@ -2,3 +2,46 @@ window.onload = function () {
     document.querySelector("#content").classList.toggle("show");
     document.getElementById("load").style.display = "none";
 }
+// To prevent default button action.
+var bu = document.querySelector("#button-one");
+bu.addEventListener("click", function(event){
+    event.preventDefault();
+});
+var bu2 = document.querySelector("#button-two");
+bu2.addEventListener("click", function(event){
+    event.preventDefault();
+});
+// To prevent default enter action.
+const form = document.querySelector("#form-one");
+form.addEventListener("keydown", function(event) {
+    if(event.key==="Enter"){
+        event.preventDefault();
+    }
+});
+
+function saveInfo() {
+    var ele = document.querySelector("#text-box");
+    var newContent = document.querySelector("#new-content");
+
+    // Create a new <p> element
+    var newParagraph = document.createElement("p");
+    newParagraph.textContent = ele.value; // Set the content safely
+
+    // Append the new paragraph to #new-content
+    newContent.appendChild(newParagraph);
+
+    // Clear the text box
+    ele.value = "";
+    // Create and append the <br> after the paragraph
+    //var lineBreak = document.createElement("br");
+    //newContent.appendChild(lineBreak);
+    var horizline = document.createElement("hr");
+    horizline.style.width = "70%";
+    newContent.appendChild(horizline);
+
+}
+
+function deletInfo() {
+    var containing = document.querySelector("#new-content");
+    containing.innerHTML = "";
+}
